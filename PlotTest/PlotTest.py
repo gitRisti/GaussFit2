88c010a274f32d6e0d28b1e8c2b8a4419f487c08
gitEldest <robert.risti@ttu.ee>
2018-12-06 17:25:55 +0200

    Started on fitting 4 and N-number of gaussians

--- PlotTest/PlotTest.py
+++ PlotTest/PlotTest.py
@@ -40,23 +40,37 @@ def gaussian(x, pars):
 
 #Show raw data and peaks
 plt.plot(values_x,values_y, label = "Raw data")
+plt.show()
+insertPeaksManually = input("Do you want to enter peaks manually? (Y/N) \n")
+if insertPeaksManually == "Y":
+    pM = [] #Initial guesses for peak mean
+    nPeaks = int(input("Please enter # of peaks:\n"))
+    for i in range(1,nPeaks+1):
+        pM.append(int(input("Enter mean for peak " + str(i) + "\n")))
+    for i in pM:
+        plt.plot(values_x[i],values_y[i],"x")
+elif insertPeaksManually == "N":
+    #Find noise (standard deviation of baseline)
+    noise = np.std(values_y[0:10])
+    #, = tuple unpacking
+    peaks, _ = (find_peaks(values_y,100))
+    for i in peaks:
+        plt.plot(values_x[i],values_y[i],"x")
+    nPeaks = peaks.shape
+    nPeaks = nPeaks[0]
+    print("Found " + str(nPeaks) + " peaks")
 
-peaks, _ = (find_peaks(values_y,100))
-for i in peaks:
-    plt.plot(values_x[i],values_y[i],"x")
-nPeaks = peaks.shape
-print("Found " + str(nPeaks) + " peaks")
+plt.plot(values_x,values_y, label = "Raw data")
 plt.show()
 
-peak1_width = input("Enter initial width(0...1) for peak 1 \n")
-peak2_width = input("Enter initial width(0...1) for peak 2 \n")
+pW = [] #Initial guesses for peak width
+peakNames = []
+
+for i in range(1,nPeaks+1):
+    pW.append(float(input("Enter initial width(0...1) for peak " + str(i) + "\n")))
 peak1_name = input("Enter name for peak1 \n")
 peak2_name = input("Enter name for peak2 \n")  
 
-peak1_width = float(peak1_width)
-peak2_width = float(peak2_width)
-
-#, = tuple unpacking
 
 
 #Define two peaks
@@ -67,14 +81,23 @@ def twoGaussians(x,height1,center1,width1,height2,center2,width2):
     f2 = gaussian(x,[height2,center2,width2])
     return f1+f2
 
+def fourGaussians(x,height1,center1,width1,height2,center2,width2,height3,center3,width3,height4,center4,width4):
+    f1 = gaussian(x,[height1,center1,width1])
+    f2 = gaussian(x,[height1,center1,width1])
+    f3 = gaussian(x,[height1,center1,width1])
+    f4 = gaussian(x,[height1,center1,width1])
+    return f1+f2+f3+f4
 gaussianTest = []
 
-guess = [values_y[peaks[0]],values_x[peaks[0]],peak1_width,values_y[peaks[1]],values_x[peaks[1]],peak2_width]
-for i in values_x:
-    gaussianTest.append(twoGaussians(i,*guess))
+if nPeaks == 2:
+    guess = [values_y[peaks[0]],values_x[peaks[0]],pW[0],values_y[peaks[1]],values_x[peaks[1]],pW[1]]
+    for i in values_x:
+        gaussianTest.append(twoGaussians(i,*guess))
+elif nPeaks == 4:
+    guess4 = [values_y[pM[0]],values_x[pM[0]],pW[0],values_y[pM[1]],values_x[pM[1]],pW[1],values_y[pM[2]],values_x[pM[2]],pW[2],values_y[pM[3]],values_x[pM[3]],pW[3]]
+    for i in values_x:
+        gaussianTest.append(fourGaussians(i,*guess))
 
-peakWidths = peak_widths(values_y,peaks,rel_height = 0.5)
-print(peakWidths)
 
 popt, pcov = curve_fit(twoGaussians, values_x, values_y, p0=[*guess])
 fit1 = popt[0:3]
@@ -88,10 +111,12 @@ area2 = (np.trapz(gauss2,values_x))
 
 #Draw raw data and final fit
 plt.plot(values_x,values_y, label = "Raw data")
-#plt.plot(values_x,gaussianTest,label="Initial fit")
-plt.plot(values_x,gauss1, label = peak1_name)
-plt.plot(values_x,gauss2, label = peak2_name)
+plt.plot(values_x,gaussianTest,label="Initial fit")
 #plt.plot(values_x, twoGaussians(values_x, *popt), label = "Final fit")
+plt.show()
+#plt.plot(values_x,values_y, label = "Raw data")
+#plt.plot(values_x,gauss1, label = peak1_name)
+#plt.plot(values_x,gauss2, label = peak2_name)
 
 #Axis manipulation
 plt.xlabel("Volume (ml)")
